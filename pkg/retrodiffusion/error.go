39de1ce449f9ee17c59326ed44252796b3193296
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-08-22 19:53:17 +0200

    Update error.go

--- pkg/retrodiffusion/error.go
+++ pkg/retrodiffusion/error.go
@@ -1,7 +1,39 @@
 package retrodiffusion
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func (e Error) Error() string {
-	return fmt.Sprintf("%s: %s", e.Detail.Code, e.Detail.Message)
+func (e APIError) Error() string {
+	b := &strings.Builder{}
+
+	for i, d := range e.Detail {
+		if i > 0 {
+			b.WriteString("; ")
+		}
+
+		for j, loc := range d.Loc {
+			if j > 0 {
+				b.WriteByte('.')
+			}
+
+			b.WriteString(loc)
+		}
+		if len(d.Loc) > 0 {
+			b.WriteString(": ")
+		}
+
+		b.WriteString(d.Msg)
+
+		if d.Type != "" {
+			fmt.Fprintf(b, " (%s)", d.Msg, d.Type)
+		}
+	}
+
+	return b.String()
 }
+
+// func (e Error) Error() string {
+// 	return fmt.Sprintf("%s: %s", e.Detail.Code, e.Detail.Message)
+// }
