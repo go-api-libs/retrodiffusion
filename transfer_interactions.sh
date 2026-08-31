c07597a0faa6eb03eaa4d19b7b71c39718bf1d58
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-08-31 15:19:11 +0200

    Update transfer_interactions.sh

--- transfer_interactions.sh
+++ transfer_interactions.sh
@@ -1,4 +1,4 @@
 cp api/interactions.json ../../MarkRosemaker/openapi-enrich/testdata/retrodiffusion/interactions.json
 
-cp api/interactions.json ../../MarkRosemaker/openapi-codegen/testdata/retrodiffusion-full/api/interactions.json
-cp api/openapi.json ../../MarkRosemaker/openapi-codegen/testdata/retrodiffusion-full/api/openapi.json
+# cp api/interactions.json ../../MarkRosemaker/openapi-codegen/testdata/retrodiffusion-full/api/interactions.json
+# cp api/openapi.json ../../MarkRosemaker/openapi-codegen/testdata/retrodiffusion-full/api/openapi.json
